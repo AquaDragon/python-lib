45eec4a30b6f7cf961a246a94a29bffbd8dd1a4a
Shawn <22651173+AquaDragon@users.noreply.github.com>
2021-03-03 20:42:29 -0800

    Update find_multiref_phase.py
    Use itertools.product to improve code readability, updated find_multiref_phase to specify ref shot channel

--- find_multiref_phase.py
+++ find_multiref_phase.py
@@ -1,7 +1,7 @@
 '''
 NAME:           find_multiref_phase.py  (IDL:find_multiref_phase.pro)
 AUTHOR:         swjtang
-DATE:           16 Jan 2021
+DATE:           03 Mar 2021
 DESCRIPTION:    Finds the phase between signals to be used in conditional
                 averaging. Only for XY planes.
 INPUTS:         data = A formatted numpy array in order of (nt,nx,ny,nshots,
@@ -15,6 +15,7 @@ importlib.reload(<module>)
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy import signal
+from itertools import product
 
 import lib.toolbox as tbx
 import lib.errorcheck as echk
@@ -28,90 +29,90 @@ def correlate_multi_signals(data, lagarray, passarray, trange=[5000, 17500],
     copyarr = np.empty([nt, nx, ny, nshots, nchan])
     corr_arr = np.zeros(data[t1:t2, :, :, 0, :].shape)
 
-    for xx in range(nx):
-        for yy in range(ny):
-            for ichan in range(nchan):
-                temp = np.zeros(t2-t1)
-                ii = 0
-                for ss in range(nshots):
-                    tbx.progress_bar([xx+1, yy+1, ss+1, ichan+1], [nx, ny,
-                                     nshots, nchan], label=['nx', 'ny',
-                                     'nshots', 'nchan'], header='Conditionally'
-                                     ' averaging data...')
-                    if passarray[xx, yy, ss, ichan] == 0:
-                        lag = int(lagarray[xx, yy, ss, ichan])
+    for xx, yy, ichan in product(range(nx), range(ny), range(nchan)):
+        tbx.progress_bar([xx, yy, ichan], [nx, ny, nchan],
+                         label=['nx', 'ny', 'nchan'],
+                         header='Conditionally averaging data...')
+        temp = np.zeros(t2-t1)
+        ii = 0
+        for ss in range(nshots):
+            if passarray[xx, yy, ss, ichan] == 0:
+                lag = int(lagarray[xx, yy, ss, ichan])
 
-                        # only filter if there is a need to extract the shot
-                        if filterflag == 1:
-                            copyarr[:, xx, yy, ss, ichan] = tbx.filterfreq(
-                                data[:, xx, yy, ss, ichan], time, ftype='high',
-                                f0=1, width=1)
-                            temp += copyarr[t1+lag:t2+lag, xx, yy, ss, ichan]
-                        else:
-                            temp += data[t1+lag:t2+lag, xx, yy, ss, ichan]
-                        ii += 1
-                if ii != 0:
-                    corr_arr[:, xx, yy, ichan] = temp/ii
+                # only filter if there is a need to extract the shot
+                if filterflag == 1:
+                    copyarr[:, xx, yy, ss, ichan] = tbx.filterfreq(
+                        data[:, xx, yy, ss, ichan], time, ftype='high',
+                        f0=1, width=1)
+                    temp += copyarr[t1+lag:t2+lag, xx, yy, ss, ichan]
+                else:
+                    temp += data[t1+lag:t2+lag, xx, yy, ss, ichan]
+                ii += 1
+        if ii != 0:
+            corr_arr[:, xx, yy, ichan] = temp/ii
     return(t1, t2, corr_arr)
 
 
-# -----------------------------------------------------------------------------
-def find_multiref_phase(data, time, ref=[0, 0, 0], trange=None, arg2pi=None,
-                        db=0, dbshot=[4, 5, 0, 1]):
-    # Extract parameters from inputs ---------------------------
-    # data dimensions
+def find_multiref_phase(data, trange=None, ref=[0, 0, 0, 0],
+                        dbshot=[0, 1, 1, 0], **kwargs):
+    ''' ----------------------------------------------------------------------
+    Function which determines the lag time of all other shots in the array
+    with respect to a reference shot.
+    INPUTS:     data = 1D array of the fixed B-dot data.
+    OPTIONAL:   trange = The start and end time of data to be correlated.
+                         If unspecificed, it uses the entire data set.
+                         Computationally intensive, so set reasonable values.
+                ref    = The index (ix, iy, ishot) of the reference shot. If
+                         unspecified, use the 1st shot of the entire dataset.
+                dbshot = The index (ix, iy, ishot, ichan) used for debugging.
+                **kwargs gets passed into determine_lag_time()
+    '''
     nt, nx, ny, nshots, nchan = data.shape
-    # reference shot ID (x, y, shot)
-    rx, ry, rs = ref[0], ref[1], ref[2]
+    rx, ry, rs, rchan = ref[0], ref[1], ref[2], ref[3]
 
-    # Error checking -------------------------------------------
+    # Error checking ---------------------------------------------------------
     if (trange is None) or (len(trange) != 2):
         print('!!! Unrecognized trange values. Requires trange=[t1, t2]')
         t1, t2 = 0, nt
     else:
         t1, t2 = echk.check_trange(nt, trange[0], trange[1])
     print('trange = {0}, {1}'.format(t1, t2))
+    # ------------------------------------------------------------------------
 
-    # start ----------------------------------------------------
-    # the array to store the correlated data
-    lagarr = np.empty([nx, ny, nshots, nchan])
-    # the array to determine if a shot should be skipped
-    passarr = np.zeros([nx, ny, nshots, nchan])
+    lagarr = np.empty([nx, ny, nshots, nchan])    # correlated data
+    passarr = np.zeros([nx, ny, nshots, nchan])   # determine if shot skipped
 
-    if db == 0:
-        for xx in range(nx):
-            for yy in range(ny):
-                for ss in range(nshots):
-                    for ichan in range(nchan):
-                        tbx.progress_bar([xx+1, yy+1, ss+1, ichan+1], [nx, ny,
-                                         nshots, nchan], label=['nx', 'ny',
-                                         'nshots', 'nchan'])
-                        refsig = data[t1:t2, rx, ry, rs, ichan]
-                        sig1 = data[t1:t2, xx, yy, ss, ichan]
+    if 'db' not in kwargs:
+        for xx, yy, ss, ichan in product(range(nx), range(ny), range(nshots),
+                                         range(nchan)):
+            # Execute for loop
+            tbx.progress_bar([xx, yy, ss, ichan], [nx, ny, nshots, nchan],
+                             label=['nx', 'ny', 'nshots', 'nchan'])
+            ref1 = data[t1:t2, rx, ry, rs, ichan]
+            sig1 = data[t1:t2, xx, yy, ss, ichan]
 
-                        laginfo = determine_lag_time(refsig, sig1,
-                                                     arg2pi=arg2pi)
-                        if laginfo['error'] == 0:
-                            lagarr[xx, yy, ss, ichan] = laginfo['xlag']
-                        elif laginfo['error'] == 1:
-                            passarr[xx, yy, ss, ichan] = 1
+            laginfo = determine_lag_time(ref1, sig1, **kwargs)
+            if laginfo['error'] == 0:
+                lagarr[xx, yy, ss, ichan] = laginfo['xlag']
+            elif laginfo['error'] == 1:
+                passarr[xx, yy, ss, ichan] = 1
+
     else:
-        ref = data[t1:t2, rx, ry, rs, dbshot[3]]
+        ref = data[t1:t2, rx, ry, rs, rchan]
         sig = data[t1:t2, dbshot[0], dbshot[1], dbshot[2], dbshot[3]]
-        temp = determine_lag_time(ref, sig, arg2pi=arg2pi, db=db)
-
+        temp = determine_lag_time(ref, sig, **kwargs)
         return 0, 0
 
     # display a figure for visualization
     plt.figure(figsize=(8, 4.5))
-    plt.plot(range(t1, t2), refsig-np.average(refsig))
+    plt.plot(range(t1, t2), ref1-np.average(ref1))
     plt.plot(range(t1-int(lagarr[xx, yy, ss, ichan]), t2-int(lagarr[xx, yy,
              ss, ichan])), sig1-np.average(sig1))
     plt.legend(['Reference signal', 'Last signal in data'])
 
     # rejection percentage
     p_reject = (passarr == 1).sum() / passarr.size
-    print('Reference shot rejection rate = {0:.2f}%'.format(p_reject*100))
+    print('Shot rejection rate = {0:.2f}%'.format(p_reject*100))
     if p_reject > 0.5:
         print('Reference shot is lousy! Choose another one.')
 

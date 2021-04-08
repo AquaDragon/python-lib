70958f4c6e1b2ebdfd7d1891f3f44d1f47c3e275
Shawn <22651173+AquaDragon@users.noreply.github.com>
2021-04-07 19:32:18 -0700

    Update find_multiref_phase.py
    Reorganize 'lagtime' function

--- find_multiref_phase.py
+++ find_multiref_phase.py
@@ -1,7 +1,7 @@
 '''
 NAME:           find_multiref_phase.py  (IDL:find_multiref_phase.pro)
 AUTHOR:         swjtang
-DATE:           03 Mar 2021
+DATE:           15 Mar 2021
 DESCRIPTION:    Finds the phase between signals to be used in conditional
                 averaging. Only for XY planes.
 INPUTS:         data = A formatted numpy array in order of (nt,nx,ny,nshots,
@@ -12,10 +12,10 @@ import importlib
 importlib.reload(<module>)
 ------------------------------------------------------------------------------
 '''
+from itertools import product
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy import signal
-from itertools import product
+import scipy
 
 import lib.toolbox as tbx
 import lib.errorcheck as echk
@@ -65,7 +65,7 @@ def find_multiref_phase(data, trange=None, ref=[0, 0, 0, 0],
                 ref    = The index (ix, iy, ishot) of the reference shot. If
                          unspecified, use the 1st shot of the entire dataset.
                 dbshot = The index (ix, iy, ishot, ichan) used for debugging.
-                **kwargs gets passed into determine_lag_time()
+                **kwargs gets passed into lagtime()
     '''
     nt, nx, ny, nshots, nchan = data.shape
     rx, ry, rs, rchan = ref[0], ref[1], ref[2], ref[3]
@@ -91,7 +91,7 @@ def find_multiref_phase(data, trange=None, ref=[0, 0, 0, 0],
             ref1 = data[t1:t2, rx, ry, rs, ichan]
             sig1 = data[t1:t2, xx, yy, ss, ichan]
 
-            laginfo = determine_lag_time(ref1, sig1, **kwargs)
+            laginfo = lagtime(ref1, sig1, **kwargs)
             if laginfo['error'] == 0:
                 lagarr[xx, yy, ss, ichan] = laginfo['xlag']
             elif laginfo['error'] == 1:
@@ -100,7 +100,7 @@ def find_multiref_phase(data, trange=None, ref=[0, 0, 0, 0],
     else:
         ref = data[t1:t2, rx, ry, rs, rchan]
         sig = data[t1:t2, dbshot[0], dbshot[1], dbshot[2], dbshot[3]]
-        temp = determine_lag_time(ref, sig, **kwargs)
+        temp = lagtime(ref, sig, **kwargs)
         return 0, 0
 
     # display a figure for visualization
@@ -120,45 +120,57 @@ def find_multiref_phase(data, trange=None, ref=[0, 0, 0, 0],
 
 
 # ----------------------------------------------------------------------------
-def determine_lag_time(sig1, sig2, arg2pi=None, db=0, threshold=0.5):
-    # ERROR CHECKING -------------------------------------------
+def lagtime(sig1, sig2, tmax=None, plot=0, threshold=0.6, quiet=0):
+    '''
+    OPTIONAL:   tmax      = The maximum number of pixels to search for the
+                            correlation length. Default value is the entire
+                            signal length.
+                plot      = Show plots used for debugging.
+                threshold = Value between 0 and 1. Used to select peaks above
+                            certain level.
+    '''
+    # ERROR CHECKING
     if len(sig1) != len(sig2):
-        print('!!! [find_multiref_phase] Length of two inputs are different!')
+        print('!!! [lagtime] Length of two inputs are different!')
         return None
-    if arg2pi is None:
-        arg2pi = len(sig1)  # set default value of search range
 
-    # PREPARING DATA: subtracting mean -------------------------
-    sig1 = sig1 - np.average(sig1)
-    sig2 = sig2 - np.average(sig2)
+    if tmax is None:
+        tmax = len(sig1)  # set default value of search range
 
-    # CONSTRUCT THE LAG ARRAY ----------------------------------
-    npoints = len(sig1)
-    xarr = [ii - npoints/2 for ii in range(npoints)]
+    npoints = len(sig1)    # Number of points
 
-    # PERFORM CROSS-CORRELATION --------------------------------
-    yarr = np.array(tbx.c_correlate(sig2, sig1))
+    # Subtract the mean from the data
+    sig1 = sig1 - np.mean(sig1)
+    sig2 = sig2 - np.mean(sig2)
+
+    # Perform cross-correlation
+    corr = np.array(tbx.c_correlate(sig2, sig1))
 
     # FIND THE LOCATION OF ALL THE PEAKS
-    if np.amax(yarr) <= threshold:
-        if db != 0:
-            print('Debug: Max correlation < threshold (={0})'.
-                  format(threshold))
-            plt.plot(yarr)
-        return {'error': 1}
+    if np.amax(corr) <= threshold:
+        tbx.qprint(quiet, '!!! Max correlation less than set threshold (= '
+                   '{0})'.format(threshold))
+        if plot != 0:
+            plt.plot(corr)
+        return {
+            'xlag':  None,
+            'error': 1
+            }
     else:
-        peak_find = signal.find_peaks(yarr.flatten(), height=threshold)
-        xpeaks_0 = np.array([xarr[int(ii)] for ii in peak_find[0]])
-        ypeaks_0 = np.array(peak_find[1]['peak_heights'])
+        peaks, props = scipy.signal.find_peaks(corr, height=threshold)
+        xpeaks_0 = peaks - int(npoints/2)
+        ypeaks_0 = props['peak_heights']
 
-        condition = np.where(abs(xpeaks_0) < arg2pi)
+        condition = np.where((xpeaks_0 < tmax/2) & (xpeaks_0 > -tmax/2))
         xpeaks = xpeaks_0[condition]
         ypeaks = ypeaks_0[condition]
 
         if len(xpeaks) == 0:
-            if db != 0:
-                print('Debug: no correlation peaks found')
-            return {'error': 1}
+            tbx.qprint(quiet, '!!! No peaks found with specified condition')
+            return {
+                'xlag':  None,
+                'error': 1
+                }
 
     # LOCATE MAXIMUM CROSS-CORRELATION PEAK, DETERMINE LAG-TIME
     ind = np.argmax(ypeaks)
@@ -167,17 +179,15 @@ def determine_lag_time(sig1, sig2, arg2pi=None, db=0, threshold=0.5):
     # RETURN THIS OUTPUT ---------------------------------------
     test = {
         'xlag':   xlag,
-
         'xpeaks': xpeaks,
         'ypeaks': ypeaks,
-        'xarr':   xarr,
-        'yarr':   yarr,
+        'corr':   corr,
         'error':  0
     }
 
-    if db != 0:
+    if plot != 0:
         plt.figure(figsize=(8, 4.5))
-        plt.plot(xarr, yarr)
+        plt.plot(np.arange(npoints)-int(npoints/2), corr)
         plt.plot(xpeaks, ypeaks, 'rx')
         plt.title('Correlation plot (debug mode)', fontsize=25)
 

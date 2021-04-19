ec02cb31e75652300d96f9043a951eb362d932b5
Shawn <22651173+AquaDragon@users.noreply.github.com>
2021-04-19 15:23:04 -0700

    Update rfea.py
    - find_Ti to return 3 outputs if correlation returns None
    - add function condavg_rfea to calculate conditionally averaged data using integrated B
    - add function calc_Ti_arr to calculate Ti for an array of values
    - add function show_bint_range to plot boundary range of B-int

--- rfea.py
+++ rfea.py
@@ -1,7 +1,7 @@
 '''
 NAME:           rfea.py
 AUTHOR:         swjtang
-DATE:           12 Mar 2021
+DATE:           19 Apr 2021
 DESCRIPTION:    A toolbox of functions related to energy analyzer analysis.
 ------------------------------------------------------------------------------
 to reload module:
@@ -17,15 +17,15 @@ import scipy
 from scipy.optimize import curve_fit
 from matplotlib import pyplot as plt
 
+import lib.find_multiref_phase as fmp
 import lib.toolbox as tbx
 import lib.spikes as spike
+
 
 ''' --------------------------------------------------------------------------
     DATA ANALYSIS FUNCTIONS
 ------------------------------------------------------------------------------
 '''
-
-
 # Find Ti from curve fitting of the decaying part of -dI/dV
 def find_Ti(xx, yy, plot=0, width=40, xmax=0, xoff=10):
     # xoff = offset used for curve fitting gaussian
@@ -88,7 +88,7 @@ def find_Ti_exp(volt, curr, plot=0, startpx=100, endpx=100, mstime=0, fid=0,
         popt, pcov = curve_fit(exp_func, volt[argmin:], temp[argmin:], p0=guess,
                                bounds=(bound_down, bound_up))
     except:
-        return None, None
+        return None, None, None
 
     Vp = volt[np.argmin(abs([start for ii in volt] - exp_func(volt, *popt)))]
     Ti = popt[1]
@@ -139,11 +139,96 @@ def mstime(time, ind, start=5, off=0):
 
 
 ''' --------------------------------------------------------------------------
+    ANALYSIS ROUTINES
+------------------------------------------------------------------------------
+'''
+def condavg_rfea(data, bdata, nsteps, nshots, trange=None, btrange=None,
+                 ref=[0, 0], bref=None):
+    ''' ----------------------------------------------------------------------
+    Conditionally avarage shift of RFEA current data.
+    INPUTS:   data    = np.array with the data to be conditionally averaged.
+              bdata   = np.array with the phase information (usually bdot)
+              nsteps  = Number of steps in the voltage sweep
+              nshots  = Number of shots for each step in the voltage sweep
+              trange  = Time range to store conditionally averaged data.
+              btrange = Time range of the conditional averaging (bdot)
+    OPTIONAL: ref = [step, shot] number of the reference shot
+              bref = Inputs a reference shot for conditional averaging
+    '''
+    if trange is not None:
+        t1, t2 = trange
+    else:
+        t1, t2 = 0, data.shape[0]
+
+    if btrange is not None:
+        bt1, bt2 = btrange
+    else:
+        bt1, bt2 = t1, t2
+
+    # Current array, shifted in phase
+    curr_arr = np.zeros((t2-t1, nsteps, nshots))
+    # Array shows number of shots skipped because cross-correlation fails
+    skip_arr = np.zeros(nsteps)
+
+    # Determine the reference shot in bdata
+    if bref is None:
+        bref = bdata[bt1:bt2, ref[0], ref[1]]
+
+    for step in range(nsteps):
+        skips = 0
+        for shot in range(nshots):
+            tbx.progress_bar([step, shot], [nsteps, nshots], ['nsteps',
+                             'nshots'])
+            bsig = bdata[bt1:bt2, step, shot]
+            xlag = fmp.lagtime(bref, bsig, quiet=1, threshold=0.7)['xlag']
+
+            if xlag is not None:
+                curr_arr[:, step, shot] = np.roll(data[t1:t2, step, shot, 0],
+                                                  -xlag)
+            else:
+                skips += 1
+        skip_arr[step] = skips
+
+    # Calculates factor so that mean_curr takes mean of shots not skipped
+    factor = [nshots/(nshots - ii) for ii in skip_arr]
+    mean_curr = np.mean(curr_arr, axis=2) * factor
+
+    return mean_curr, curr_arr, bref
+
+
+def calc_Ti_arr(volt, curr, res=1, trange=None, dt=1, **kwargs):
+    ''' ----------------------------------------------------------------------
+    Calculate Ti and Vp from an array of RFEA IV curves
+    INPUTS:   volt = np.array of voltage data
+              curr = np.array of current data
+    OPTIONAL: res    = [Ohm] Resistance used to measure current
+              trange = [start, stop] time indices
+              dt     = Number of indices to skip 
+    '''
+    if trange is not None:
+        t1, t2 = trange
+    else:
+        t1, t2 = 0, curr.shape[0]
+
+    tarr = np.arange(t1, t2, dt)
+    nt = tarr.shape[0]
+
+    # Define arrays
+    Ti = np.empty(nt)
+    Vp = np.empty(nt)
+    errTi = np.empty(nt)
+
+    for ii in range(nt):
+        tt = tarr[ii]
+        tbx.progress_bar([ii], [nt])
+        Vp[ii], Ti[ii], errTi[ii] = find_Ti_exp(volt, curr[tt,:]/res, plot=0,
+                                                save=0, **kwargs)
+    return Ti, Vp, errTi
+
+''' --------------------------------------------------------------------------
     PLOTTING FUNCTIONS
 ------------------------------------------------------------------------------
 '''
-
-
 # Plots the current derivative -dI/dV (if input is deriv do a regular plot)
 def plot_IVderiv(volt, curr, xoff=0, yoff=0, nwindow=51, polyn=3, **kwargs):
     plt.plot(volt + xoff, IVderiv(curr, nwindow=nwindow, polyn=polyn) +
@@ -172,13 +257,24 @@ def browse_data(data, x=None, y=None, step=0, shot=0, chan=0, trange=[0,-1]):
 
     return t1, t2
 
+# Plot function to show the bounded region of integrated B used for condavg
+def show_bint_range(data, trange=None, fid=0):
+    tbx.prefig(xlabel='time [px]', ylabel='B-int')
+    plt.plot(data)
+    if trange is not None:
+        t1, t2 = trange
+        plt.plot([t1,t1], [np.min(data), np.max(data)], 'orange')
+        plt.plot([t2,t2], [np.min(data), np.max(data)], 'orange')
+        tbx.savefig('./img/{0}-condavg-range.png'.format(fid))
+    else:
+        t1, t2 = 0, len(data)
+    return t1, t2
+
 
 ''' --------------------------------------------------------------------------
     NAMING FUNCTIONS
 ------------------------------------------------------------------------------
 '''
-
-
 def fname_gen(series, date='2021-01-28', folder='/data/swjtang/RFEA/', ch=2):
     # Plots the current derivative -dI/dV
     return '{0}{1}/C{2}{3}.txt'.format(folder, date, ch, series)
@@ -188,8 +284,6 @@ def fname_gen(series, date='2021-01-28', folder='/data/swjtang/RFEA/', ch=2):
     BASIC MATH FUNCTIONS
 ------------------------------------------------------------------------------
 '''
-
-
 def gauss_func(x, a, b, c, x0):
     return a * np.exp(-b * (np.sqrt(x)-np.sqrt(x0))**2) + c
 
